583422e58988c93636a5133a99344b33736f6438
Pierre B. Samson <pierre.b.samson@gmail.com>
2018-11-25 20:30:57 -0500

    Second edition

--- app/src/main/java/com/pbsi2/sfquiz/MainActivity.java
+++ app/src/main/java/com/pbsi2/sfquiz/MainActivity.java
@@ -1,29 +1,170 @@
 package com.pbsi2.sfquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    int xcore = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView myview = (TextView) findViewById(R.id.answeredTextView);
+        myview.setText("1");
+        // Start fragment transaction
+        FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        fragQuiz.replace(R.id.question_holder, new Quest1());
+        // could also add, but not in this
+        // Finish and display
+        fragQuiz.commit();
     }
-    boolean firstQuestion(View view){
-      int ansid = view.getId();
-      RadioButton rightans = (RadioButton) findViewById(R.id.ans4RadioButton);
-      if (rightans.getId() == ansid){
-          String good = "\"" + rightans.getText() + "\" is the right answer (10 " + this.getString(R.string.score) + ")";
-          Toast.makeText(getApplicationContext(), good, Toast.LENGTH_LONG).show();
-          return true;
-      } else {
-          String bad = "WRONG: \"" + rightans.getText() + "\" is the right answer (0 " + this.getString(R.string.score) + ")";
-          Toast.makeText(getApplicationContext(), bad, Toast.LENGTH_LONG).show();
-          return false;
-      }
+
+    public void firstQuestion(View view) {
+        int ansid = view.getId();
+        RadioButton rightans = (RadioButton) findViewById(R.id.ans4RadioButton);
+        if (rightans.getId() == ansid) {
+            String good = "\"" + rightans.getText() + "\" is the right answer (10 " + this.getString(R.string.score) + ")";
+            xcore += 10;
+            setXcore(xcore);
+            Toast.makeText(getApplicationContext(), good, Toast.LENGTH_LONG).show();
+
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest2());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("2");
+        } else {
+            String bad = "WRONG:\"" + rightans.getText() + "\" is the right answer (0 " + this.getString(R.string.score) + ")\nRETRY____>";
+            Toast.makeText(getApplicationContext(), bad, Toast.LENGTH_LONG).show();
+
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest1());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("1");
+        }
+
+    }
+
+    public void secondQuestion(View view) {
+
+        CheckBox rightans = (CheckBox) findViewById(R.id.first_checkBox);
+        CheckBox rightans2 = (CheckBox) findViewById(R.id.second_checkBox);
+        CheckBox rightans3 = (CheckBox) findViewById(R.id.third_checkBox);
+        CheckBox rightans4 = (CheckBox) findViewById(R.id.forth_checkBox);
+        if (rightans.isChecked() && rightans2.isChecked() && !rightans3.isChecked() && !rightans4.isChecked()) {
+            String good = "\"" + rightans.getText();
+            good += "\n\"" + rightans2.getText();
+            good += "\" is the right answer (20 " + this.getString(R.string.score) + ")";
+            xcore += 20;
+            setXcore(xcore);
+            Toast.makeText(getApplicationContext(), good, Toast.LENGTH_LONG).show();
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest3());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("3");
+        } else {
+            String bad = "WRONG: \"" + rightans.getText();
+            bad += "\n\"" + rightans2.getText();
+            bad += "\" is the right answer (20 " + this.getString(R.string.score) + ")RETRY____>";
+            Toast.makeText(getApplicationContext(), bad, Toast.LENGTH_LONG).show();
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest2());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("2");
+        }
+
+    }
+
+    public void thirdQuestion(View view) {
+
+        TextView rightans = (TextView) findViewById(R.id.ans3_1_textView);
+        if (rightans.getText().toString().equalsIgnoreCase(getString(R.string.ans3_1))) {
+            String good = "\"" + rightans.getText().toString() + "\" is the right answer (10 " + this.getString(R.string.score) + ")";
+            xcore += 20;
+            setXcore(xcore);
+            Toast.makeText(getApplicationContext(), good, Toast.LENGTH_LONG).show();
+            
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest4());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("4");
+        } else {
+            String bad = "WRONG: \"" + getString(R.string.ans3_1) + "\" is the right answer (0 " + this.getString(R.string.score) + ")RETRY____>";
+            Toast.makeText(getApplicationContext(), bad, Toast.LENGTH_LONG).show();
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the frame with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest3());
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("3");
+        }
+
+
+    }
+
+    public void forthQuestion(View view) {
+        int ansid = view.getId();
+        RadioButton rightans = (RadioButton) findViewById(R.id.ans4_3_RadioButton);
+        if (rightans.getId() == ansid) {
+            String good = "GOOD: \"" + rightans.getText() + "\" is the right answer (10 " + this.getString(R.string.score) + ")";
+            xcore += 10;
+            setXcore(xcore);
+            Toast.makeText(getApplicationContext(), good, Toast.LENGTH_LONG).show();
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest1());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("1(New)");
+            setXcore(0);
+        } else {
+            String bad = "WRONG: \"" + rightans.getText() + "\" is the right answer (0 " + this.getString(R.string.score) + ")RETRY____>";
+            Toast.makeText(getApplicationContext(), bad, Toast.LENGTH_LONG).show();
+            FragmentTransaction fragQuiz = getSupportFragmentManager().beginTransaction();
+            // Replace the contents of the container with the new fragment
+            fragQuiz.replace(R.id.question_holder, new Quest4());
+            // could also add, but not in this
+            // Finish and display
+            fragQuiz.commit();
+            TextView myview = (TextView) findViewById(R.id.answeredTextView);
+            myview.setText("4");
+        }
+
+    }
+
+    public void setXcore(int xcore) {
+        String score = String.valueOf(xcore);
+        TextView scoreview = (TextView) findViewById(R.id.scoreTextView);
+        scoreview.setText(score);
     }
 }
---
+++ app/src/main/java/com/pbsi2/sfquiz/quest1.java
@@ -0,0 +1,30 @@
+package com.pbsi2.sfquiz;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Quest1 extends Fragment {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.first_question, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+
+    }
+
+}
